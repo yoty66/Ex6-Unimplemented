4de6328d8c32e60661d1596580a4883ecdec13f2
royshafir <royshafir8@gmail.com>
2019-06-13 18:03:41 +0300

    changed Material class to support light

--- src/edu/cg/models/Car/Materials.java
+++ src/edu/cg/models/Car/Materials.java
@@ -4,39 +4,46 @@ import com.jogamp.opengl.GL2;
 
 public class Materials {
 	// TODO: Use this class to update the color of the vertexes when drawing.
-	private static final float DARK_GRAY[] = { 0.2f, 0.2f, 0.2f };
-	private static final float DARK_RED[] = { 0.25f, 0.01f, 0.01f };
-	private static final float RED[] = { 0.7f, 0f, 0f };
-	private static final float BLACK[] = { 0.05f, 0.05f, 0.05f };
+	private static final float DARK_GRAY[] = { 0.2f, 0.2f, 0.2f,1 };
+	private static final float DARK_RED[] = { 0.25f, 0.01f, 0.01f,1 };
+	private static final float RED[] = { 0.7f, 0f, 0f,1 };
+	private static final float BLACK[] = { 0.05f, 0.05f, 0.05f,1 };
 
 	public static void SetMetalMaterial(GL2 gl, float[] color) {
 		gl.glColor3fv(color, 0);
 	}
 
-	public static void SetBlackMetalMaterial(GL2 gl) {
-		SetMetalMaterial(gl, BLACK);
+	public static void SetBlackMetalMaterial(GL2 gl){
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, BLACK, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
+
 	}
 
 	public static void SetRedMetalMaterial(GL2 gl) {
-		SetMetalMaterial(gl, RED);
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, RED, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
 	}
 
 	public static void SetDarkRedMetalMaterial(GL2 gl) {
-		SetMetalMaterial(gl, DARK_RED);
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, DARK_RED, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
 	}
 	
 	public static void SetDarkGreyMetalMaterial(GL2 gl) {
-		SetMetalMaterial(gl, DARK_GRAY);
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, DARK_GRAY, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
 	}
 
 	public static void setMaterialTire(GL2 gl) {
-		float col[] = { .05f, .05f, .05f };
-		gl.glColor3fv(col,0);
+		float col[] = { .05f, .05f, .05f,1 };
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, col, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
 	}
 
 
 	public static void setMaterialRims(GL2 gl) {
-		gl.glColor3fv(DARK_GRAY,0);
+		gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, DARK_GRAY, 0);
+		gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 10f);
 	}
 
 
